e87591c37958650e2aae4e1b12d23d6661b15e35
angel reginaldo paucar <125907278+i4ngel@users.noreply.github.com>
2023-11-01 18:12:49 -0500

    Update conex.php

--- Controller/conex.php
+++ Controller/conex.php
@@ -1,8 +1,8 @@
 <?php
 $host= "localhost";
-$user= "id21473201_admin"; //EL USUARIO DE TU BD
-$password="Angel3216@"; //LA CONTRASEÑA DE TU BD
-$bd="id21473201_personal";//EL NOMBRE DE LA BD
+$user= ""; //EL USUARIO DE TU BD
+$password=""; //LA CONTRASEÑA DE TU BD
+$bd="";//EL NOMBRE DE LA BD
 
 $conexion=mysqli_connect($host,$user,$password,$bd);
 
